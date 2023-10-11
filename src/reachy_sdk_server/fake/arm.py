debfe95ec3c1e3f81d8bfca58059d4db801da471
glannuzel <gaelle.lannuzel@gmail.com>
2023-10-11 11:13:56 +0200

    Add fake answers to the arm IK FK

--- src/reachy_sdk_server/fake/arm.py
+++ src/reachy_sdk_server/fake/arm.py
@@ -18,14 +18,23 @@ from reachy_sdk_api_v2.arm_pb2 import (
     SpeedLimit,
     SpeedLimitRequest,
     ArmTemperatures,
+    ArmIKRequest,
+    ArmFKRequest,
+    ArmFKSolution,
 )
 from reachy_sdk_api_v2.arm_pb2_grpc import ArmServiceServicer
 from reachy_sdk_api_v2.part_pb2 import PartInfo, PartId
 from reachy_sdk_api_v2.orbita2d_pb2 import (
     Orbita2DInfo,
+    Pose2D,
 )
 from reachy_sdk_api_v2.orbita3d_pb2 import (
     Orbita3DInfo,
+)
+from reachy_sdk_api_v2.kinematics_pb2 import (
+    Rotation3D,
+    ExtEulerAngles,
+    Matrix4x4,
 )
 from .orbita2d import FakeOrbita2D
 from .orbita3d import FakeOrbita3D
@@ -52,11 +61,25 @@ class ArmServicer(ArmServiceServicer):
                 ) for arm in self.arms.values()]
         )
 
-    def ComputeArmFK(self, request, context):
-        return super().ComputeArmFK(request, context)
+    def ComputeArmFK(self, request: ArmFKRequest, context) -> ArmFKSolution:
+        fk_sol = ArmFKSolution(
+            success=True,
+            end_effector=ArmEndEffector(pose=Matrix4x4(
+                data=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+            ))
+        )
+        return fk_sol
 
-    def ComputeArmIK(self, request, context):
-        return super().ComputeArmIK(request, context)
+    def ComputeArmIK(self, request: ArmIKRequest, context) -> ArmIKSolution:
+        ik_sol = ArmIKSolution(
+            success=True,
+            arm_position=ArmPosition(
+                shoulder_position=Pose2D(axis_1=1, axis_2=2),
+                elbow_position=Pose2D(axis_1=3, axis_2=4),
+                wrist_position=Rotation3D(rpy=ExtEulerAngles(roll=5, pitch=6, yaw=7))
+            )
+        )
+        return ik_sol
 
     def GoToCartesianPosition(self, request, context):
         return super().GoToCartesianPosition(request, context)
