e3549bda941ae9e1352201930cedd62387154bb3
Simon Le Goff <32677536+simheo@users.noreply.github.com>
2023-12-13 11:23:30 +0100

    Check null value for mobile base in config file.

--- reachy_sdk_server/reachy_sdk_server/grpc_server/mobile_base.py
+++ reachy_sdk_server/reachy_sdk_server/grpc_server/mobile_base.py
@@ -95,7 +95,7 @@ class MobileBaseServicer(
             "version_soft": config["mobile_base"]["version_soft"],
         }
 
-        if config["mobile_base"]["serial_number"] == "None":
+        if not config["mobile_base"]["serial_number"]:
             self.logger.info("No mobile base found in the config file. Mobile base server not initialized.")
             return
 
