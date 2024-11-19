00551d75cd292a9937bc5a2689121e947a6a8528
steve <steve.nguyen.000@gmail.com>
2024-11-19 12:39:54 +0100

    remove nan and inf, ensure we do not overflow

--- reachy_sdk_server/reachy_sdk_server/grpc_server/video_server.py
+++ reachy_sdk_server/reachy_sdk_server/grpc_server/video_server.py
@@ -191,10 +191,14 @@ class ReachyGRPCVideoSDKServicer:
 
         if cam_type == CameraType.DEPTH and self._gazebo_mode:
             data = np.frombuffer(data, dtype=np.float32)  # specific conversion from Gazebo 32FC1 (in m) to 16UC1 (in mm)
+            data=data.astype(np.float64)
             data = data * 1000.0
+            data = np.nan_to_num(data)
+            data = np.clip(data, 0, 65535)
             data = data.astype(np.uint16)
             data = data.tobytes()
             encoding = "16UC1"
+
         frame = ROSFrameRaw(
             timestamp=Timestamp(seconds=msg.header.stamp.sec, nanos=msg.header.stamp.nanosec),
             data=data,
